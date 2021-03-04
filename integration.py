7364074e57bd68edc3a5c473d170ec82a0a6b755
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-03-04 16:16:24 -0500

    coded double integrals

--- integration.py
+++ integration.py
@@ -2,15 +2,19 @@
 # TODO: comment this file
 import numpy as np
 
-from guassian_quadrature import GuassianQuadrature
+from gaussian_quadrature import GaussianQuadrature
 
 
 class Integration:
     def __init__(self, weights, func) -> None:
         self.weights = weights
+        if func is None:
+            return
         if isinstance(func, np.ndarray):
             self.func_values = func
         elif isinstance(func, tuple):
+            if func[1] is None:
+                return
             assert isinstance(func[0], np.ndarray)
             assert callable(func[1])
             self.func_values = np.vectorize(func[1])(func[0])
@@ -26,13 +30,29 @@ class Integration:
     def __str__(self) -> str:
         return str(self.value)
 
+class Integration2D(Integration):
+    def __init__(self, method1, method2, N, func, a=-1, b=1, c=-1, d=1) -> None:
+        self.int1 = method1(N, None, a=a, b=b)
+        self.int2 = method2(N, None, a=c, b=d)
+        weights2d = np.outer(self.int1.weights, self.int2.weights)
+        self.weights = np.reshape(weights2d, weights2d.size)
+        func_values = []    
+        for x in self.int1.xs:
+            for y in self.int2.xs:
+                func_values.append(func(x, y))
+        self.func_values = np.array(func_values)
+        super().__init__(self.weights, self.func_values)
 
-class GuassianQuadratureIntegration(Integration):
+class GaussianQuadratureIntegration(Integration):
     def __init__(self, N, func, a=-1, b=1, method="closed form") -> None:
-        self.G = GuassianQuadrature(N, method=method)
+        self.G = GaussianQuadrature(N, method=method)
         mid = (a + b) / 2
         dist = (b - a) / 2
-        self.func = np.vectorize(lambda x: dist * func(dist * x + mid))
+        if func != None:
+            self.func = np.vectorize(lambda x: dist * func(dist * x + mid))
+        else:
+            self.func = None
+        self.xs = self.G.x
         super().__init__(self.G.w, (self.G.x, self.func))
 
 
@@ -42,8 +62,8 @@ class TrapezoidRule(Integration):
         weights = np.ones(N + 1) * h
         weights[0] = h / 2
         weights[-1] = h / 2
-        xs = np.arange(a, b + h, h)
-        super().__init__(weights, (xs, func))
+        self.xs = np.arange(a, b + h, h)
+        super().__init__(weights, (self.xs, func))
 
 
 class SimpsonsRule(Integration):
@@ -53,8 +73,8 @@ class SimpsonsRule(Integration):
         weights = np.ones(N + 1) * h / 3
         for i in range(1, N):
             weights[i] *= 4 if i % 2 == 1 else 2
-        xs = np.arange(a, b + h, h)
-        super().__init__(weights, (xs, func))
+        self.xs = np.arange(a, b + h, h)
+        super().__init__(weights, (self.xs, func))
 
 
 class Simpsons38Rule(Integration):
@@ -66,8 +86,8 @@ class Simpsons38Rule(Integration):
         weights[-1] *= 3
         for i in range(1, N):
             weights[i] *= 6 if i % 3 == 0 else 9
-        xs = np.arange(a, b + h, h)
-        super().__init__(weights, (xs, func))
+        self.xs = np.arange(a, b + h, h)
+        super().__init__(weights, (self.xs, func))
 
 
 class BoolesRule(Integration):
@@ -79,20 +99,26 @@ class BoolesRule(Integration):
         weights[-1] *= 14
         for i in range(1, N):
             weights[i] *= 64 if i % 2 == 1 else (24 if i % 4 == 2 else 28)
-        xs = np.arange(a, b + h, h)
-        super().__init__(weights, (xs, func))
+        self.xs = np.arange(a, b + h, h)
+        super().__init__(weights, (self.xs, func))
 
 
 if __name__ == "__main__":
+    print("------------- Integration of x^2 --------------------")
     N = 600
     a = -6
     b = 2
     f = lambda x: x * x
     integrals = [
-        GuassianQuadratureIntegration(N, f, a=a, b=b, method="closed form").value,
+        GaussianQuadratureIntegration(N, f, a=a, b=b, method="closed form").value,
         TrapezoidRule(N, f, a=a, b=b).value,
         SimpsonsRule(N, f, a=a, b=b).value,
         Simpsons38Rule(N, f, a=a, b=b).value,
         BoolesRule(N, f, a=a, b=b).value,
     ]
     print(integrals)
+    print("------------- 2D Integration of x^2 + y^2 --------------------")
+    N = 600
+    f = lambda x, y: x*x + y*y
+    integral = Integration2D(GaussianQuadratureIntegration, BoolesRule, N, f) # going over (-1, 1) in x and y
+    print(integral)
